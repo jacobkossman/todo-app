af4bcbfda20405e96b7318e71e800a1456a92b8b
pip_lineupbuilder <howley.phil@gmail.com>
2016-07-22 22:53:05 -0700

    update with fake todo array data

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@ angular.module( 'todoListApp', [] )
     $scope.helloWorld = function() {
       console.log( "Hello from the helloWorld controller function, in the mainCtrl" );
     };
+
+    $scope.todos = [
+      { "name": "clear car" },
+      { "name": "feed dog" },
+      { "name": "buy groceries" },
+      { "name": "exercise" },
+      { "name": "read a chapter of a book" },
+      { "name": "wake up early" }
+    ];
   } );
